a72c2991a1f1ab288c3f1ee21221dbe8156fef76
vlad1705 <vpasemko@gmail.com>
2021-02-08 15:32:53 +0200

    added h2 & h3

--- src/HoverCounter.js
+++ src/HoverCounter.js
@@ -5,6 +5,8 @@ function HoverCounter(props) {
     return (
         <>
             <h1 onMouseEnter={props.func}>You entered {props.count} times</h1>
+            <h2 onMouseEnter={props.func}>You entered {props.count} times</h2>
+            <h3 onMouseEnter={props.func}>You entered {props.count} times</h3>
         </>
     )
 }
