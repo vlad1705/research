fd8db0a1a5e01489fb1d718603e0311fa6f762be
vlad1705 <vpasemko@gmail.com>
2021-02-08 16:16:46 +0200

    changed h1 in hovercounter

--- src/HoverCounter.js
+++ src/HoverCounter.js
@@ -4,7 +4,7 @@ import HoCounter from './HoCounter'
 function HoverCounter(props) {
     return (
         <>
-            <h1>Another branch</h1>
+            <h1>Modifited by another way</h1>
             <h1 onMouseEnter={props.func}>You entered {props.count} times</h1>
         </>
     )
