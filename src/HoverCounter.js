6869d8c7e23c6a4182d2a15f1cca23c26738508a
Vladyslav <vpasemko@gmail.com>
2021-02-08 14:15:59 +0200

    added text in hovercounter

--- src/HoverCounter.js
+++ src/HoverCounter.js
@@ -3,7 +3,10 @@ import HoCounter from './HoCounter'
 
 function HoverCounter(props) {
     return (
-        <h1 onMouseEnter={props.func}>You entered {props.count} times</h1>
+        <>
+            <h1>Another branch</h1>
+            <h1 onMouseEnter={props.func}>You entered {props.count} times</h1>
+        </>
     )
 }
 
